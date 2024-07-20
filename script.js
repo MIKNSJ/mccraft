09c4aae7471d068c0b0ae0763b81b72273e4a27f
MIKNSJ <sculetoncode@gmail.com>
2024-07-19 19:36:41 -0700

    improvements to header

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function clickNavButton() {
-    document.getElementsByClassName("nav_bar_list")[0].classList.toggle("show");
+    document.getElementsByClassName("nav_menu")[0].classList.toggle("show_nav_menu");
     document.getElementById("nav_icon").classList.toggle("change_button");
 }
 
 window.onclick = function(event) {
     if (!event.target.matches(".nav_button")) {
-        var elements = document.getElementsByClassName("nav_bar_list");
+        var elements = document.getElementsByClassName("nav_menu");
         for (var i = 0; i < elements.length; i++) {
             var element = elements[i];
-            if (element.classList.contains("show")) {
-                element.classList.remove("show");
+            if (element.classList.contains("show_nav_menu")) {
+                element.classList.remove("show_nav_menu");
             }
         }
 
